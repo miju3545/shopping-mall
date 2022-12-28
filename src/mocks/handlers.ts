3400bb81acaaab55c6e16e8b780a4be7332162bc
rovxxmjxx <miju3545@gmail.com>
2022-12-28 17:27:35 +0900

    refactor: dummy image url 수정하기

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -6,7 +6,7 @@ import { EXECUTE_PAY } from '../graphql/payment';
 const mockProducts = (() =>
   Array.from({ length: 100 }, (_, i) => ({
     id: `${i + 1}`,
-    imageUrl: `https://placeimg.com/640/480/${i + 1}`,
+    imageUrl: `https:/picsum.photos/id/${i + 10}/200/150`,
     price: 5000,
     title: `임시상품${i + 1}`,
     createdAt: new Date(1671965475944 * (i * 1000 * 60 * 60 * 24)).toString(),
